7ee3c743487f5054e75211c6e7e23f335b8e9163
Eric Anderson <ericrayanderson@Erics-MacBook-Pro.local>
2018-02-18 21:28:06 -0500

    v1

--- simpListsV1/ViewController.swift
+++ simpListsV1/ViewController.swift
@@ -6,20 +6,82 @@
 //  Copyright © 2018 Eric Anderson. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //the json file url
+    let URL_HEROES = "http://ericrayanderson.pythonanywhere.com/json_test";
+    
+    //A string array to save all the names
+    var nameArray = [String]()
+    
+    //the label we create
+    @IBOutlet weak var labelTest: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //calling the function that will fetch the json
+        getJsonFromUrl();
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //this function is fetching the json from URL
+    func getJsonFromUrl(){
+        //creating a NSURL
+        let url = NSURL(string: URL_HEROES)
+        
+        //fetching the data from the url
+        URLSession.shared.dataTask(with: (url as? URL)!, completionHandler: {(data, response, error) -> Void in
+            
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                
+                //printing the json in console
+                print(jsonObj!.value(forKey: "avengers")!)
+                
+                //getting the avengers tag array from json and converting it to NSArray
+                if let heroeArray = jsonObj!.value(forKey: "avengers") as? NSArray {
+                    //looping through all the elements
+                    for heroe in heroeArray{
+                        
+                        //converting the element to a dictionary
+                        if let heroeDict = heroe as? NSDictionary {
+                            
+                            //getting the name from the dictionary
+                            if let name = heroeDict.value(forKey: "name") {
+                                
+                                //adding the name to the array
+                                self.nameArray.append((name as? String)!)
+                            }
+                            
+                        }
+                    }
+                }
+                
+                OperationQueue.main.addOperation({
+                    //calling another function after fetching the json
+                    //it will show the names to label
+                    self.showNames()
+                })
+            }
+        }).resume()
+    }
+    
+    func showNames(){
+        //looing through all the elements of the array
+        for name in nameArray{
+            
+            //appending the names to label
+            labelTest.text = labelTest.text! + name + "\n";
+        }
+    }
+    
+    
 }
-
